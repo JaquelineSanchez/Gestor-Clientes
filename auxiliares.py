b655f382f1f8de7017d161ea6334a31212af09d5
Andrea Sanchez <l19141196@queretaro.tecnm.mx>
2022-07-20 15:12:52 -0500

    funciones listas: limpieza y lectura

--- auxiliares.py
+++ auxiliares.py
@@ -1,1 +1,13 @@
 #Funciones auxiliares para uso general
+import os
+import platform
+
+def limpiarPantalla():
+    os.system('cls') if platform.system() == "Windows" else os.system('clear')
+
+def leerTexto(min=0,max=100,mensaje=None):
+    print(mensaje) if mensaje else None
+    while True:
+        texto = input(">")
+        if min <= len(texto) <=max:
+            return texto
