f0f40f8506909dab5d44721266c1c7ac37c2830f
Andrea Sanchez <l19141196@queretaro.tecnm.mx>
2022-07-26 11:54:40 -0500

    Interfaz estructurada

--- interfaz.py
+++ interfaz.py
@@ -1,5 +1,7 @@
 """ Menú gráfico"""
+import database as db
 from tkinter import *
+from tkinter import ttk
 
 class CenterMixin:
 
@@ -25,12 +27,45 @@ class Main(Tk, CenterMixin):
         self.center()
     
     def build(self):
-        button = Button(self,text = "Hola", command = self.hola)
-        button.pack()
+        frame = Frame(self)
+        frame.pack()
+        
+        #Configurar tabla
+        treeview = ttk.Treeview(frame)
+        treeview['columns'] = ('DNI','NOMBRE','APELLIDO')        
 
-    def hola(self):
-        print("Hola mundo!")
+        treeview.column("#0",width=0,stretch=NO)
+        treeview.column("DNI", anchor=CENTER)
+        treeview.column("NOMBRE", anchor=CENTER)
+        treeview.column("APELLIDO", anchor=CENTER)
 
+        treeview.heading("DNI",text="DNI", anchor=CENTER)
+        treeview.heading("NOMBRE",text="NOMBRE", anchor=CENTER)
+        treeview.heading("APELLIDO",text="APELLIDO", anchor=CENTER)
+        
+        #añadir scroll
+        scrollbar = Scrollbar(frame)
+        scrollbar.pack(side=RIGHT, fill=Y)        
+        treeview['yscrollcommand'] = scrollbar.set
+
+        #cargar informacion
+        for cliente in db.Clientes.lista:
+            treeview.insert(
+                parent='', index ='end', iid=cliente.dni,
+                values=(cliente.dni, cliente.nombre, cliente.apellido))
+        treeview.pack()
+
+        #botones
+        frame = Frame(self)
+        frame.pack(pady=20)
+
+        Button(frame,text="Crear",command=None).grid(row=0,column=0)
+        Button(frame,text="Modificar",command=None).grid(row=0,column=1)
+        Button(frame,text="Borrar",command=None).grid(row=0,column=2)
+
+        #atributo de instancia
+        self.treeview = treeview
+    
 if __name__ == "__main__":
     app = Main()
     app.mainloop()
